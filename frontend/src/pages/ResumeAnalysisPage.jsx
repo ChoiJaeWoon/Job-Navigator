df586756ab3ef68d994b5f79b7c2f3062be2346f
sjs4273 <sjs4273@naver.com>
2025-06-19 17:18:03 +0900

    feat: PDF 분석 페이지 및 업로드 UI 추가

--- frontend/src/pages/ResumeAnalysisPage.jsx
+++ frontend/src/pages/ResumeAnalysisPage.jsx
@@ -1,12 +1,33 @@
+import React from 'react';
 import Header from '../components/Header';
+import { useNavigate } from 'react-router-dom';
+import "./ResumeAnalysisPage.css";
 
 function ResumeAnalysisPage() {
+  const navigate = useNavigate();
+
+  const goToJobPage = () => {
+    navigate('/job-analysis'); // 원하는 경로로 수정
+  };
+
   return (
     <div>
       <Header />
-      <main style={{ padding: '2rem' }}>
-        <h2>이력서 분석 페이지</h2>
-      </main>
+
+      <div className="tab-bar">
+        <button className="tab active2">PDF분석</button>
+        <button className="tab2" onClick={goToJobPage}>직무분석</button>
+      </div>
+
+      <div className="pdf-upload-container">
+        <input
+          type="file"
+          accept="application/pdf"
+          className="pdf-input"
+          placeholder="PDF파일을 올려주세요"
+        />
+        <button className="pdf-upload-button">분석시작</button>
+      </div>
     </div>
   );
 }
